e9aef1f871688a1a0f7ce1e8cf696ba5b9d1242a
PearUK <muicamau007@gmail.com>
2021-08-14 00:52:26 +0700

    first up

---
+++ js/api.js
@@ -0,0 +1,146 @@
+getCovidWorld_case();
+getCovidWorld_recovered();
+getCovidWorld_death();
+getCovidWorld_active();
+
+function getCovidWorld_case(){
+	fetch('https://api-kent.netlify.app/.netlify/functions/api/global/all')
+	.then(res => res.json())
+	.then(data => {
+		console.log(data);
+
+		let nguoinhiem = data.cases;
+		document.getElementById("case_world").innerHTML = nguoinhiem.toLocaleString("en");
+
+	});
+}
+
+function getCovidWorld_recovered(){
+	fetch('https://api-kent.netlify.app/.netlify/functions/api/global/all')
+	.then(res => res.json())
+	.then(data => {
+		console.log(data);
+
+		let phuchoi = data.recovered;
+		document.getElementById("recovered_world").innerHTML = phuchoi.toLocaleString("en");
+
+	});
+}
+
+function getCovidWorld_death(){
+	fetch('https://api-kent.netlify.app/.netlify/functions/api/global/all')
+	.then(res => res.json())
+	.then(data => {
+		console.log(data);
+
+		let tuvong = data.deaths;
+		document.getElementById("death_world").innerHTML = tuvong.toLocaleString("en");
+
+	});
+}
+
+function getCovidWorld_active(){
+	fetch('https://api-kent.netlify.app/.netlify/functions/api/global/all')
+	.then(res => res.json())
+	.then(data => {
+		console.log(data);
+
+		let dangchua = data.active;
+		document.getElementById("active_world").innerHTML = dangchua.toLocaleString("en");
+
+	});
+}
+
+getCovidVn_case();
+getCovidVn_recovered();
+getCovidVn_death();
+getCovidVn_active();
+
+function getCovidVn_case(){
+	fetch('https://api.zingnews.vn/public/v2/corona/getChart?type=stats')
+	.then(res => res.json())
+	.then(data => {
+		console.log(data);
+
+		let nguoinhiem = data.data.all.total;
+		document.getElementById("case_vn").innerHTML = nguoinhiem.toLocaleString("en");
+
+	});
+}
+
+function getCovidVn_recovered(){
+	fetch('https://api.zingnews.vn/public/v2/corona/getChart?type=stats')
+	.then(res => res.json())
+	.then(data => {
+		console.log(data);
+
+		let phuchoi = data.data.all.recover;
+		document.getElementById("recovered_vn").innerHTML = phuchoi.toLocaleString("en");
+
+	});
+}
+
+function getCovidVn_death(){
+	fetch('https://api.zingnews.vn/public/v2/corona/getChart?type=stats')
+	.then(res => res.json())
+	.then(data => {
+		console.log(data);
+
+		let tuvong = data.data.all.death;
+		document.getElementById("death_vn").innerHTML = tuvong.toLocaleString("en");
+
+	});
+}
+
+function getCovidVn_active(){
+	fetch('https://api.zingnews.vn/public/v2/corona/getChart?type=stats')
+	.then(res => res.json())
+	.then(data => {
+		console.log(data);
+
+		let dangchua = data.data.all.remain;
+		document.getElementById("active_vn").innerHTML = dangchua.toLocaleString("en");
+
+	});
+}
+
+getVaccineVn_total();
+getVaccineVn_one();
+getVaccineVn_two();
+
+function getVaccineVn_total(){
+	fetch('https://api.zingnews.vn/public/v2/corona/getChart?type=vaccination')
+	.then(res => res.json())
+	.then(data => {
+		console.log(data);
+
+		let totalvct = data.data.first.total + data.data.second.total;
+
+		document.getElementById("total_vaccinated").innerHTML = totalvct.toLocaleString("en");
+
+	});
+}
+
+function getVaccineVn_one(){
+	fetch('https://api.zingnews.vn/public/v2/corona/getChart?type=vaccination')
+	.then(res => res.json())
+	.then(data => {
+		console.log(data);
+
+		let onevct = data.data.first.total;
+		document.getElementById("one_vaccinated").innerHTML = onevct.toLocaleString("en");
+
+	});
+}
+
+function getVaccineVn_two(){
+	fetch('https://api.zingnews.vn/public/v2/corona/getChart?type=vaccination')
+	.then(res => res.json())
+	.then(data => {
+		console.log(data);
+
+		let twovct = data.data.second.total;
+		document.getElementById("two_vaccinated").innerHTML = twovct.toLocaleString("en");
+
+	});
+}
---
+++ js/scroll.js
@@ -0,0 +1,42 @@
+window.smoothScroll = function(target) {
+    var scrollContainer = target;
+    do { //find scroll container
+        scrollContainer = scrollContainer.parentNode;
+        if (!scrollContainer) return;
+        scrollContainer.scrollTop += 1;
+    } while (scrollContainer.scrollTop == 0);
+    
+    var targetY = 0;
+    do { //find the top of target relatively to the container
+        if (target == scrollContainer) break;
+        targetY += target.offsetTop;
+    } while (target = target.offsetParent);
+    
+    scroll = function(c, a, b, i) {
+        i++; if (i > 30) return;
+        c.scrollTop = a + (b - a) / 30 * i;
+        setTimeout(function(){ scroll(c, a, b, i); }, 10);
+    }
+    // start scrolling
+    scroll(scrollContainer, scrollContainer.scrollTop, targetY, 0);
+}
+
+//Get the button scroll top
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
